e254f83bd76ca22ad9a24d871347fcc44a8e2be4
Makar <113338407+MikeBabki@users.noreply.github.com>
2024-01-12 22:48:35 +0300

    TestCommit

---
+++ Lesson_1.java
@@ -0,0 +1,16 @@
+public class Lesson_1 {
+    public static void main(String[] args) {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else System.out.println("Сумма отрицательная");
+    }
+}
