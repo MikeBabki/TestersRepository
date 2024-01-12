18a15c7fef32123e0a1bbdf3692fc3ff74185f11
Makar <113338407+MikeBabki@users.noreply.github.com>
2024-01-12 23:08:44 +0300

    Lesson 1 complete

--- Lesson_1.java
+++ Lesson_1.java
@@ -1,16 +1,41 @@
 public class Lesson_1 {
     public static void main(String[] args) {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+        firstTask();
+        secondTask();
+        thirdTask();
+        fourthTask();
     }
-
-    public static void checkSumSign() {
-        int a = 5;
+    public  static  void firstTask() {
+    System.out.println("Orange");
+    System.out.println("Banana");
+    System.out.println("Apple");
+}
+    public static void secondTask() {
+        int a = -6;
         int b = 3;
         int sum = a + b;
         if (sum >= 0) {
             System.out.println("Сумма положительная");
         } else System.out.println("Сумма отрицательная");
+    }
+    public  static  void thirdTask() {
+        int value = 210;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+    public  static  void  fourthTask() {
+        int a = 12;
+        int b = 10;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 }
