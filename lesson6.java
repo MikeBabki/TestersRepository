59d5a9358bc813f825fc29fa6239825ac1ecaea4
Makar <113338407+MikeBabki@users.noreply.github.com>
2024-02-07 21:52:56 +0300

    lesson 6

---
+++ lesson6.java
@@ -0,0 +1,49 @@
+public class lesson6 {
+
+    public static void main(String[] args) {
+        String[][] array = {
+                {"1", "2", "3", "5"},
+                {"6", "6", "2", "1"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            int result = sumArrayElements(array);
+            System.out.println("Sum of array elements: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static int sumArrayElements(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Array size is not 4x4");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Invalid data at cell (" + i + "," + j + ")");
+                }
+            }
+        }
+
+        return sum;
+    }
+}
+
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
